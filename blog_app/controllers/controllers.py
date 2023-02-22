6f8ae7765ee96b1056d49c54f7131eed95f2a1ca
obabawale <padinality@yahoo.com>
2023-02-22 11:54:54 +0100

    add controllers to handle api calls

--- blog_app/controllers/controllers.py
+++ blog_app/controllers/controllers.py
@@ -1,21 +1,53 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http, SUPERUSER_ID
+from odoo.http import request, Response
+import json
 
 
-# class BlogApp(http.Controller):
-#     @http.route('/blog_app/blog_app/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class BlogApp(http.Controller):
 
-#     @http.route('/blog_app/blog_app/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('blog_app.listing', {
-#             'root': '/blog_app/blog_app',
-#             'objects': http.request.env['blog_app.blog_app'].search([]),
-#         })
+    @http.route('/api/blogs/test', auth='public', methods=['GET'], cors="*", csrf=False)
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/blog_app/blog_app/objects/<model("blog_app.blog_app"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('blog_app.object', {
-#             'object': obj
-#         })
+    def _serialize_blog(self, blogs):
+        return blogs.read(['id', 'name', 'content', 'state'])
+
+    @http.route('/api/blogs', type='http', auth="none", methods=['POST', 'GET'], cors="*", csrf=False)
+    def blog_list(self, *args, **kwargs):
+        Blog = request.env['blog_app.blog'].with_user(
+            SUPERUSER_ID)
+        blogs = []
+        errors = []
+        if request.httprequest.method == "GET":
+            blogs_data = request.env['blog_app.blog'].with_user(
+                SUPERUSER_ID).search([])
+            blogs = self._serialize_blog(blogs_data)
+        if request.httprequest.method == "POST":
+            print("------------- args ---------", args)
+            print("--------- kwargs ----", kwargs)
+            print("--------- request data ----", request.httprequest.data)
+            title = kwargs.get("title")
+            content = kwargs.get("content")
+            blog_vals = {'name': title, "content": content}
+            try:
+                blog = Blog.create(blog_vals)
+                blogs = self._serialize_blog(blog)
+            except Exception as e:
+                errors.append("Error creating content %s" % e)
+            if errors:
+                error_message = (",").join(errors)
+                return Response(str(error_message), status=500)
+
+        return http.request.make_response(
+            data=json.dumps({"blogs": blogs}),
+            headers=[('Content-Type', 'application/json')]
+        )
+
+    @http.route('/api/blogs/<model("blog_app.blog"):blog>', type='http', auth="none", methods=['UPDATE', 'PATCH', 'DELETE'], cors="*", csrf=False)
+    def blog_detail(self, blog, *args, **kwargs):
+        print("=======blog=======", blog)
+        return http.request.make_response(
+            data=json.dumps({"result": "sucess"}),
+            headers=[('Content-Type', 'application/json')]
+        )
